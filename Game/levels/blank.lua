ce7f25087b4697ce7f8b6f317a0e89c21fbfb426
Simon Hoke <simonhoke@gmail.com>
2013-12-17 18:18:46 -0800

    trying to fix images folder part 2

---
+++ Game/levels/blank.lua
@@ -0,0 +1,6 @@
+function load()
+	world = love.physics.newWorld(0, 9.81*64, true)
+	
+end
+
+return load
---
+++ Game/levels/menu.lua
@@ -0,0 +1,32 @@
+function load()
+	love.window.setIcon(love.image.newImageData("images/icon.png"))
+	world = love.physics.newWorld(0, 9.81*64, true)
+
+	objects = {}
+
+	objects.background = {}
+	objects.background.body = love.physics.newBody(world, settings.window.width/2, (settings.window.height/2), "static")
+	objects.background.shape = love.physics.newRectangleShape(settings.window.width, settings.window.height)
+	objects.background.fixture = love.physics.newFixture(objects.background.body, objects.background.shape)
+	objects.background.draw = backgroundDraw
+	objects.background.click = backgroundClick
+end
+
+function backgroundDraw()
+	love.graphics.setColor(255,255,255)
+	love.graphics.polygon("fill", objects.background.body:getWorldPoints(objects.background.shape:getPoints()))
+
+	love.graphics.setColor(0,0,0)
+	line = "Telekinetic Bunny!"
+	love.graphics.print(line, getCenterCoords(line, settings.window.width/2, settings.window.width*1.5, "x"), 20)
+end
+
+function backgroundClick()
+	loadLevel("test")
+end
+
+function updateLevel()
+
+end
+
+return load

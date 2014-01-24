fe7691e97d4ccf6d1bcd12075cd573dc5a3cb699
Simon Hoke <simonhoke@gmail.com>
2014-01-23 22:22:40 -0800

    scientist has joints!

--- Game/objects/scientist.lua
+++ Game/objects/scientist.lua
@@ -1,10 +1,11 @@
 function joinScientist()
 	local joints = {}
-	joints.head = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_head.body, 349, 585, false)
-	joints.leftarm = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_leftarm.body, 305, 594, false)
-	joints.rightarm = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_rightarm.body, 379, 596, false)
-	joints.leftleg = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_leftleg.body, 329, 649, false)
-	joints.rightleg = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_rightleg.body, 375, 649, false)
+	joints.head = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_head.body, 392, 589, false)
+	joints.head2 = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_head.body, 321, 583, false)
+	joints.leftarm = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_leftarm.body, 308, 614, false)
+	joints.rightarm = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_rightarm.body, 460, 636, false)
+	joints.leftleg = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_leftleg.body, 344, 725, false)
+	joints.rightleg = love.physics.newRevoluteJoint(objects.scientist_torso.body, objects.scientist_rightleg.body, 447, 720, false)
 	objects.scientist_torso.joints = joints
 end
 
@@ -59,33 +60,33 @@ objects.scientist_torso = {
 	click = function() end,
 }
 objects.scientist_head = {
-	body = love.physics.newBody(world, 270, settings.window.height-615, "dynamic"),
+	body = love.physics.newBody(world, 250, settings.window.height-725, "dynamic"),
 	shape = love.physics.newPolygonShape(43.0*2,4.7*2, 95.0*2,6.3*2, 124.5*2,37.1*2, 117.0*2,117.9*2, 81.6*2,120.5*2, 30.8*2,111.3*2, 19.6*2,100.3*2, 19.4*2,27.7*2),
 	draw = function() end,
 	click = function() end,
 }
 objects.scientist_leftarm = {
-	body = love.physics.newBody(world, 300-70, settings.window.height-500, "dynamic"),
+	body = love.physics.newBody(world, 159, 596, "dynamic"),
 	shape = love.physics.newPolygonShape(20.4*2,7.2*2, 78.6*2,0, 79.4*2,28.3*2, 35.0*2,39.5*2, 17.6*2,45.3*2, 2.0*2,33.9*2, .8*2,24.0*2, 12.5*2,9.8*2),
 	draw = function() end,
 	click = function() end,
 }
 objects.scientist_rightarm = {
-	body = love.physics.newBody(world, 300+70, settings.window.height-500, "dynamic"),
-	shape = love.physics.newPolygonShape(10.3*2,.9*2, 61.2*2,27.6*2, 70.1*2,46.2*2, 66.0*2,55.8*2, 51.7*2,60.*28, 0,29.0*2),
+	body = love.physics.newBody(world, 430, 600, "dynamic"),
+	shape = love.physics.newPolygonShape(10.3*2,.9*2, 61.2*2,27.6*2, 70.1*2,46.2*2, 66.0*2,55.8*2, 51.7*2,60.8*2, 0,29.0*2),
 	draw = function() end,
 	click = function() end,
 }
 objects.scientist_leftleg = {
-	body = love.physics.newBody(world, 300, settings.window.height-435, "dynamic"),
+	body = love.physics.newBody(world, 294, 707, "dynamic"),
 	shape = love.physics.newPolygonShape(4.2*2,2.8*2, 42.3*2,.6*2, 41.4*2,44.3*2, 45.3*2,50.8*2, 46.8*2,63.3*2, 9.4*2,64.2*2, .5*2,61.1*2, 7.9*2,27.5*2),
 	draw = function() end,
 	click = function() end,
 }
 objects.scientist_rightleg = {
-	body = love.physics.newBody(world, 355, settings.window.height-445, "dynamic"),
+	body = love.physics.newBody(world, 408, 697, "dynamic"),
 	shape = love.physics.newPolygonShape(.9*2,3.8*2, 28.4*2,.7*2, 38.5*2,21.6*2, 44.3*2,43.3*2, 47.8*2,48.3*2, 52.6*2,66.2*2, 11.4*2,67.9*2, 10.7*2,31.3*2),
 	draw = function() end,
 	click = function() end,
 }
---joinScientist()
+joinScientist()

8d58ee707ed80c1784b47284b503f8ea28473549
Simon Hoke <simonhoke@gmail.com>
2014-03-19 21:05:57 -0700

    fix table

--- Game/objects/table.lua
+++ Game/objects/table.lua
@@ -3,7 +3,7 @@ tableSprite = love.graphics.newImage("images/table.png")
 function loadObject(uid)
 	local table = {
 		body = love.physics.newBody(world, 1220,750, "dynamic"),
-		shape = love.physics.newPolygonShape(4,31, 189,1, 317,51, 303,169, 115,224, 18,138),
+		shape = love.physics.newPolygonShape(4,31, 189,1, 317,51, 316,220, 4,220),
 		draw = function()
 			--love.graphics.polygon("line", objects["table"][uid].body:getWorldPoints(objects["table"][uid].shape:getPoints()))
 			love.graphics.draw(tableSprite, objects["table"][uid].body:getX(), objects["table"][uid].body:getY(), objects["table"][uid].body:getAngle())
